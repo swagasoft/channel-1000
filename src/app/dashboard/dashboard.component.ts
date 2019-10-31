a06cad99e7623dea0cea4b79596d0a68e87ba4f9
Simon Jerry <swagasoft@gmail.com>
2019-10-31 22:40:24 +0100

    project update on payment section

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,46 +12,22 @@ export class DashboardComponent implements OnInit {
   userDetails: any;
 accountDetails: any;
 userRole: any;
+referal_count: any;
+activate: any;
+
 
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
-    this.userService.loadBalance().subscribe(
-      res => {
-        this.accountDetails = res['doc'];
-        console.log(this.userDetails);
-      }
-    );
+    this.getDashboardInformations();
+    this.alignWindow();
 
     this.userRole =  this.userService.getUserRole();
     console.log('USER-ROLE',  this.userRole);
-
-    this.router.events.subscribe((evt) => {
-      if(!(evt instanceof NavigationEnd)){
-        return ;
-      }
-
-      window.scrollTo(0,0);
-    });
-    this.userService.getUserProfile().subscribe(
-      res => {
-        this.userDetails = res['user'];
-        console.log(this.userDetails);
-      localStorage.setItem('userEmail', this.userDetails.email);
-      localStorage.setItem('Username',this.userDetails.username);
-
-      },
-      err => {
-        console.log('error getting values..', err);
-
-      }
-    );
-
     // load script
     this.loadScript('../../assets/dashboard/vendor/jquery-3.2.1.min.js');
     this.loadScript('../../assets/dashboard/vendor/bootstrap-4.1/popper.min.js');
     this.loadScript('../../assets/dashboard/vendor/animsition/animsition.min.js');
-    // this.loadScript('../../assets/dashboard/vendor/select2/select2.min.js');
     this.loadScript('../../assets/dashboard/js/main.js');
 
 }
@@ -67,5 +43,32 @@ loadScript(url: string){
 }
 louOut() {
   this.userService.logout();
+}
+alignWindow(){
+  this.router.events.subscribe((evt) => {
+    if(!(evt instanceof NavigationEnd)){
+      return ;
+    }
+
+    window.scrollTo(0,0);
+  });
+}
+
+getDashboardInformations(){
+  this.userService.getUserProfile().subscribe(
+    res => {
+      this.userDetails = res['user'];
+      console.log(this.userDetails);
+      this.activate = this.userDetails.activate;
+      console.log('ACTIVATE', this.activate);
+    localStorage.setItem('userEmail', this.userDetails.email);
+    localStorage.setItem('Username',this.userDetails.username);
+
+    },
+    err => {
+      console.log('error getting values..', err);
+
+    }
+  );
 }
 }
